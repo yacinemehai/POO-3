de59d9251d1ccf5036886db063e65d828f9aa634
yacinemehai <yacine.mehai@gmail.com>
2020-11-09 12:31:52 +0100

    first commit

---
+++ Car.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once 'Vehicle.php';
+
+class Car extends Vehicle
+{
+    /**
+     * @var string
+     */
+    private $energy;
+
+    /**
+     * @var int
+     */
+    private $energyLevel;
+
+    const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->setEnergy($energy);
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): Car
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+        return $this;
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+}
